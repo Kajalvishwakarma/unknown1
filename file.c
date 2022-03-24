f3e77f74efd6e7e1e9156e352e963c2c24f0bdbe
Kajalvishwakarma <96942536+Kajalvishwakarma@users.noreply.github.com>
2022-03-24 13:18:13 +0530

    file.c
    this is c programming

---
+++ file.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int len,bred;
+  float area;
+  printf("Enter length and breadth of rectangle");
+  scanf("%d %d",&len,&bred");
+  area = len*bred;
+  printf("Area of rectangle is : %f",area);
+  return 0;
+        }            
